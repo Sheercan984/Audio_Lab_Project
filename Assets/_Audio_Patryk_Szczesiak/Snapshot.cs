1d844f450e3828a52f04caf593af527c956d533c
Patryk Szczesiak <p.szczesiak984@gmail.com>
2021-02-14 15:16:34 +0100

    Initial Commit

---
+++ Assets/_Audio_Patryk_Szczesiak/Snapshot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Snapshot : MonoBehaviour
+{
+
+    [SerializeField] AudioMixerSnapshot Outside;
+    [SerializeField] AudioMixerSnapshot Inside;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Inside.TransitionTo(1f);    
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Outside.TransitionTo(1f);
+    }
+}
